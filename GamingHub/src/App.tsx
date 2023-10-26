dce052579a565b4a568ae06780ee1974a9695449
Erik Lozanov <wolfierecordss@gmail.com>
2023-10-26 13:10:12 +0300

    Added trending component

--- GamingHub/src/App.tsx
+++ GamingHub/src/App.tsx
@@ -2,6 +2,7 @@ import Features from './components/Features.js';
 import Footer from './components/Footer.js';
 import Header from './components/Header.jsx';
 import MainBanner from './components/MainBanner.js';
+import Trending from './components/Trending.js';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
      <Header />
       <MainBanner />
       <Features />
+      <Trending />
      <Footer />
     </>
   )
