3ef24d4a72ba3e3bd08ed91f2400681c6a902140
Erik Lozanov <wolfierecordss@gmail.com>
2023-10-26 13:03:10 +0300

    Added Preloader

---
+++ GamingHub/src/components/Preloader.tsx
@@ -0,0 +1,19 @@
+export default function Preloader() {
+
+    return (
+        <>
+                    {/* ***** Preloader Start ***** */}
+                    <div id="js-preloader" className="js-preloader">
+                <div className="preloader-inner">
+                    <span className="dot" />
+                    <div className="dots">
+                        <span />
+                        <span />
+                        <span />
+                    </div>
+                </div>
+            </div>
+            {/* ***** Preloader End ***** */}
+        </>
+    );
+};
