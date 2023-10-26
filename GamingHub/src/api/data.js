e3c0f17c22f9015aad5c8c95d5c69a934f115ada
Erik Lozanov <wolfierecordss@gmail.com>
2023-10-26 14:52:04 +0300

    Update data.js

--- GamingHub/src/api/data.js
+++ GamingHub/src/api/data.js
@@ -9,27 +9,27 @@ export const register = api.register;
 export const logout = api.logout;
 
 // Implement application-specific requests
-export async function getFurniture() {
-    return await api.get(host + '/data/furnitures');
+export async function getGame() {
+    return await api.get(host + '/data/games');
 }
 
 export async function getItemById(id) {
-    return await api.get(host + '/data/furnitures/' + id);
+    return await api.get(host + '/data/games/' + id);
 }
 
-export async function getMyFurniture() {
+export async function getMyGame() {
     const userId = sessionStorage.getItem('userId');
-    return await api.get(host + `/data/furnitures?where=_ownerId%3D%22${userId}%22`);
+    return await api.get(host + `/data/games?where=_ownerId%3D%22${userId}%22`);
 }
 
 export async function createRecord(data) {
-    return await api.post(host + '/data/furnitures', data);
+    return await api.post(host + '/data/games', data);
 }
 
 export async function editRecord(id, data) {
-    return await api.put(host + '/data/furnitures/' + id, data);
+    return await api.put(host + '/data/games/' + id, data);
 }
 
 export async function deleteRecord(id) {
-    return await api.del(host + '/data/furnitures/' + id);
+    return await api.del(host + '/data/games/' + id);
 }
