823732b515a4c63f10d8b619455fea765010a644
Erik Lozanov <wolfierecordss@gmail.com>
2023-10-27 12:16:32 +0300

    Added login page

---
+++ Gaming Hub/src/components/SignIn/Login.jsx
@@ -0,0 +1,17 @@
+export default function Login() {
+
+    return(
+        <div className="page-heading header-text">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <h3>Add Game</h3>
+              <span className="breadcrumb">
+                <Link to="/">Home</Link> &gt; <Link to="/games">Games</Link> &gt; Add Game
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
