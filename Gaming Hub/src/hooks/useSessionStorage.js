1e7068ffd1cdb5a15e86529dc7d823e3d2ac67b2
Erik Lozanov <wolfierecordss@gmail.com>
2023-10-29 12:55:21 +0200

    Fixed data not saving while page refresh

--- Gaming Hub/src/hooks/useSessionStorage.js
+++ Gaming Hub/src/hooks/useSessionStorage.js
@@ -4,15 +4,12 @@ export default function useSessionStorage (key, initialValue) {
   
     const [state, setState] = useState(() => {
         const persistedStateSerialized = sessionStorage.getItem(key);
-
         if(persistedStateSerialized) {
             const persistedState = JSON.parse(persistedStateSerialized);
-
-            return;
+            return persistedState;
         }
         return initialValue;
     });
-
     const setSessionStorageState = (value) => {
         setState(value);
 
