45975e596f8a0c8c555296e9c393bfb28bc5713a
Erik Lozanov <122800063+ErikLozanov@users.noreply.github.com>
2023-11-19 13:11:11 +0200

    Added react bootstrap

--- Gaming Hub/src/App.jsx
+++ Gaming Hub/src/App.jsx
@@ -1,5 +1,8 @@
 import { Routes, Route, useNavigate } from 'react-router-dom';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home/Home';
