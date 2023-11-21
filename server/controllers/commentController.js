fca2811e862ac0a3dd3791e9ccc4a254417bceea
Erik Lozanov <122800063+ErikLozanov@users.noreply.github.com>
2023-11-21 21:32:20 +0200

    Added comment controller

---
+++ server/controllers/commentController.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+const commentManager = require("../managers/commentManager");
+
+router.get('/:gameId',async (req, res) => {
+    const game = await commentManager.getAllForGame(req.params.gameId);
+
+    res.json(game);
+});
