27e22b976eb082b754d5fd47776f92d134b5b5fd
Erik Lozanov <122800063+ErikLozanov@users.noreply.github.com>
2023-11-21 21:32:28 +0200

    Added route to comments

--- server/routes.js
+++ server/routes.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userController = require('./controllers/userController');
 const gameController = require('./controllers/gameController');
+const commentController = require('./controllers/commentController');
 
 router.use('/users', userController);
 router.use('/data/games', gameController);
+router.use('/data/comments', commentController);
 
 module.exports = router;
