bbba552a87eae00abc55181b8b520817ef52f0dd
Erik Lozanov <122800063+ErikLozanov@users.noreply.github.com>
2023-11-22 16:10:35 +0200

    Added dateFormatter util

---
+++ Gaming Hub/src/utils/dateFormatter.js
@@ -0,0 +1,12 @@
+export default function formatDate(date) {
+    const options = {
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true
+    };
+  
+    return date.toLocaleString('en-US', options).replace(",", "");
+  }
