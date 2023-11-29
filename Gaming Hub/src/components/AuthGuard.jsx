3906a395cc534368058caf84a024e6e0d873b56c
Erik Lozanov <122800063+ErikLozanov@users.noreply.github.com>
2023-11-29 13:08:59 +0200

    Create AuthGuard.jsx

---
+++ Gaming Hub/src/components/AuthGuard.jsx
@@ -0,0 +1,13 @@
+import { Navigate, Outlet} from 'react-router-dom';
+
+import {useAuthContext} from '../contexts/AuthContext';
+
+export default function AuthGuard (){ 
+    const { isAuthenticated } = useAuthContext();
+
+    if (!isAuthenticated) {
+        return <Navigate to='/users/login' />;
+    }
+
+    return <Outlet />;
+};
