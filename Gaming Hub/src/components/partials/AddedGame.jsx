80ef5a3e4b4abe0d7167ba14ff76617050341db5
Erik Lozanov <122800063+ErikLozanov@users.noreply.github.com>
2023-11-29 16:47:49 +0200

    Added partial for games in my profile

---
+++ Gaming Hub/src/components/partials/AddedGame.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+
+export default function AddedGame({game}) {
+    
+
+    return (
+        <div className="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 adv">
+        <div className="item">
+          <div className="thumb">
+            <Link to={`/games/details/${game._id}`}>
+              <img src={game.img} alt="" />
+            </Link>
+          </div>
+          <div className="down-content">
+            <span className="category">{game.category}</span>
+            <h4>{game.title}</h4>
+          </div>
+        </div>
+      </div>
+    );
+};
