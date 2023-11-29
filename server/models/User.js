2b2e15ccdc4190ac941d75ff66a840c9d02e7f2e
Erik Lozanov <122800063+ErikLozanov@users.noreply.github.com>
2023-11-30 00:20:46 +0200

    Added profilePicture and desc property to schema

--- server/models/User.js
+++ server/models/User.js
@@ -18,6 +18,14 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Password is required!'],
 
     },
+    profilePicture: {
+        type: String,
+        default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+    },
+    description: {
+        type: String,
+        default: 'No description added yet.'
+    }
 });
 
 userSchema.virtual('repeatPassword')
