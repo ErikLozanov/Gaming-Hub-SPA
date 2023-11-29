da8b3f046d2f089a760a9756fb1217956cca0aa5
Erik Lozanov <122800063+ErikLozanov@users.noreply.github.com>
2023-11-30 00:20:31 +0200

    fixed cors options on server

--- server/index.js
+++ server/index.js
@@ -13,7 +13,14 @@ mongoose.connect('mongodb://127.0.0.1:27017/games')
 app.use(express.urlencoded({extended: false}));
 // express.json will get AJAX requests (JSON data)
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: 'http://localhost:5173',
+    credentials: true,
+  };
+  
+  app.use(cors(corsOptions));
+
 // app.use(auth);
 
 // app.use((req, res, next) => {
