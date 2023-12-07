6d8b2bc51b722930e83b7924d55975fe2f830aef
Erik Lozanov <122800063+ErikLozanov@users.noreply.github.com>
2023-12-07 23:36:16 +0200

    Update useForm.jsx

--- Gaming Hub/src/hooks/useForm.jsx
+++ Gaming Hub/src/hooks/useForm.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
 
 
-
 export default function useForm (initialValues, onSubmitHandler) {
     const [values, setValues] = useState(initialValues);
-
     const changeHandler = (e) => {
         setValues(state => ({...state, [e.target.name]: e.target.value}));
     };
